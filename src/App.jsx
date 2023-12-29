358d34a639005257b6b2cc5d0add9e8d0cfe7f18
Aviral <chauhanaviral79@gmail.com>
2023-12-29 16:38:14 +0530

    components separated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React, { useEffect } from 'react'
-import Countries from './components/countries'
+import React, {useState, useEffect } from 'react'
+// import Countries from './components/countries'
 import NavBar from './components/NavBar'
 import FeatureBar from './components/FeatureBar'
-let countryData;
+const API_URL="https://restcountries.com/v3.1/all"
+
 function App() {
-  
+  let [country,setCountry]=useState([])
+  const fetchData=()=>{
+    fetch(API_URL).then((data)=>data.json()).then((data)=>{setCountry(data)})
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+  // console.log(country)
   // console.log(countryData)
   return (
     <div>
       {/* <h2>Creating react app</h2> */}
       <NavBar/>
-      {/* <FeatureBar/> */}
-      <Countries/>
+      <FeatureBar country={country}/>
+      {/* <Countries/> */}
     </div>
   )
 }
--- src/components/FeatureBar.jsx
+++ src/components/FeatureBar.jsx
@@ -1,25 +1,104 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './FeatureBar.css';
-// import Countries from './countries'
+import Countries from './countries.jsx'
 // let rawData;
+// let filteredData;
 export default function FeatureBar(props) {
-    const{country,setCountry}=props
+    const{country}=props
+    let subRegionData=[];
+    let regionData=[];
+    const [filteredData,setFilteredData]=useState(country)
+    const [search,setSearch]=useState("");
+    const [selectedRegion,setSelectedRegion]=useState("All")
+    const [selectedSubRegion,setSelectedSubRegion]=useState("All")
+    const [selectedSort,setSelectedSort]=useState("All")
+    const [selectedAreaSort,setSelectedAreaSort]=useState("All")
     // rawData=country
     // const [searchCountry,setSearchCountry]=useState("")
-    const handleSearch = (e) => {
-        const searchValue = e.target.value.toLowerCase();
-        // console.log(e.target.value)
-        if (searchValue === "") {
-          console.log(country)
-        } else {
-            const filteredData = country.filter((item) =>
-            item.name.common.toLowerCase().includes(searchValue)
-          );
-          setCountry(filteredData);
+    // filteredData=country
+    // const handleSearch = (e) => {
+      // let filteredData;
+        // const searchValue = e.target.value.toLowerCase();
+        // console.log(country)
+      //      setFilteredData(country.filter((item) =>
+      //       item.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+      //       ))
+      // };
+      // const handleRegion=(event)=>{
+        // setSelectedRegion(event.target.value)
+        // console.log(event.target.value)
+      //   if(event.target.value==="All"){
+      //     setFilteredData(country)
+      //   }else{
+      //   setFilteredData(country.filter((item)=>
+      //     item.region === event.target.value
+      //   ))}
+      // }
+      // const handleRegionChange=(e)=>{
+      //   setSelectedRegion(e.target.value)
+        // handleRegion(e)
+      // }
+      useEffect(()=>{
+        const filtered=country.filter((country)=>{
+          const searchFilter=country.name.common.toLowerCase().includes(search.toLowerCase())
+          const regionFilter=selectedRegion==='All'||country.region===selectedRegion
+          const subRegionFilter=selectedSubRegion==='All'||country.subregion===selectedSubRegion
+          return searchFilter && regionFilter && subRegionFilter
+        })
+        setFilteredData(filtered)
+      },[country,search,selectedRegion,selectedSubRegion])
+  
+      const handleSearch=(e)=>{
+        setSearch(e.target.value)
+      }
+      const handleRegionChange=(e)=>{
+        setSelectedRegion(e.target.value)
+        setSelectedSubRegion("All")
+      }
+      const handleSubRegionChange=(event)=>{
+        setSelectedSubRegion(event.target.value)
+      }
+      function handleSorting(event){
+        const sortValue=event.target.value
+        if(sortValue=="Ascending"){
+          filteredData.sort((a,b)=>a.population-b.population)
+        }else if(sortValue=="Descending"){
+          filteredData.sort((a,b)=>b.population-a.population)
         }
-      };
-      
+        setSelectedSort(sortValue)
+      }
+      function handleAreaSorting(event){
+        const areaSortValue=event.target.value
+        if(areaSortValue=="Ascending"){
+          filteredData.sort((a,b)=>a.area-b.area)
+        }else if(areaSortValue=="Descending"){
+          filteredData.sort((a,b)=>b.area-a.area)
+        }
+        setSelectedAreaSort(areaSortValue)
+      }
+      function region(){
+        country.forEach((item)=>{
+          if(!regionData.includes(item.region)){
+          regionData.push(item.region)
+          }
+        })
+      }
+      region()
+      function subRegion(){
+        country.forEach((item)=> {
+          if(item.region==selectedRegion){
+          if(selectedRegion!=="All"){
+            if(!subRegionData.includes(item.subregion)){
+            subRegionData.push(item.subregion)
+            }
+          }
+        }
+        })
+      }
+      subRegion()
+      console.log(subRegionData)
   return (
+    <>
     <section className="feature-bar">
       <div className="search-feature">
         <i className="fa-solid fa-magnifying-glass"></i>
@@ -31,18 +110,29 @@ export default function FeatureBar(props) {
           onChange={(e)=>handleSearch(e)}
         />
       </div>
-
-      <div className="dropdown">
+        <div>
+          <select value={selectedSubRegion} className='drop-select' onChange={(event)=>handleSubRegionChange(event)} >
+            <option value="All" defaultValue={selectedSubRegion} className='dropbtncontent'>
+              Filter by Subregion..
+              </option>
+              {subRegionData.map((item,index)=>{
+               return <option key={index}>{item}</option>
+              })}
+          </select>
+        </div>
+        
+      {/* <div className="dropdown">
         <select
-          value="select"  
+          value={selectedRegion}
           className="drop-select"
+          onChange={(event)=>handleRegionChange(event)}
         >
           <option
             value="All"
             className="dropbtncontent"
-            selected
+            defaultValue={selectedRegion}
           >
-            Filter By region 
+            Filter By Sub-Region 
           </option>
           <option value="Africa" className="dropbtncontent">Africa</option>
           <option value="Americas" className="dropbtncontent">Americas</option>
@@ -50,21 +140,62 @@ export default function FeatureBar(props) {
           <option value="Europe" className="dropbtncontent">Europe</option>
           <option value="Oceania" className="dropbtncontent">Oceania</option>
         </select>
-        {/* <div className="dropdown-content"> 
+      </div> */}
+      <div className="dropdown">
+        <select
+          value={selectedSort}
+          className="drop-select"
+          onChange={(event)=>handleSorting(event)}
+        >
           <option
             value="All"
             className="dropbtncontent"
-            selected
+            defaultValue={selectedSort}
           >
-            Filter by region
+            Sort According to Population
           </option>
-          <option value="Africa" className="dropbtncontent">Africa</option>
-          <option value="Americas" className="dropbtncontent">Americas</option>
-          <option value="Asia" className="dropbtncontent">Asia</option>
-          <option value="Europe" className="dropbtncontent">Europe</option>
-          <option value="Oceania" className="dropbtncontent">Oceania</option>
-        </div> */}
+          <option className="dropbtncontent">Ascending</option>
+          <option className="dropbtncontent">Descending</option>
+          </select>
+          </div>
+      <div className="dropdown">
+        <select
+          value={selectedAreaSort}
+          className="drop-select"
+          onChange={(event)=>handleAreaSorting(event)}
+        >
+          <option
+            value="All"
+            className="dropbtncontent"
+            defaultValue={selectedAreaSort}
+          >
+            Sort According to Area
+          </option>
+          <option className="dropbtncontent">Ascending</option>
+          <option className="dropbtncontent">Descending</option>
+          </select>
+          </div>
+        
+      <div className="dropdown">
+        <select
+          value={selectedRegion}
+          className="drop-select"
+          onChange={(event)=>handleRegionChange(event)}
+        >
+          <option
+            value="All"
+            className="dropbtncontent"
+            defaultValue={selectedRegion}
+          >
+            Filter By region 
+          </option>
+          {regionData.map((item,index)=>{
+               return <option key={index}>{item}</option>
+              })}
+        </select>
       </div>
     </section>
+      <Countries filteredData={filteredData}/>
+      </>
   );
 }
